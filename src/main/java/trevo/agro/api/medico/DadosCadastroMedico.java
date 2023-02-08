79a04b65afdc6642ce4bcde918d4244e6421425b
Doyddin <doydoprofissional@outlook.com>
2023-02-08 13:24:19 -0300

    Integração de Validação dos campos obrigatórios

--- src/main/java/trevo/agro/api/medico/DadosCadastroMedico.java
+++ src/main/java/trevo/agro/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,25 @@
 package trevo.agro.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import trevo.agro.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull @Valid
+        DadosEndereco endereco) {
 
 }
